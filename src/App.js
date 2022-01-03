b757e2dc44a185924799cae0d0e15d26cdf1b72b
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-01-03 12:35:00 +0000

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios';
+import {useEffect, useState} from "react"
 function App() {
+  const [users, setUsers] = useState([])
+  const[text, setText] = useState("")
+  const [suggestions, setSuggestions] = useState([])
+
+  useEffect(() => {
+    const loadUsers = async()=> {
+      const response = await axios.get("https://reqres.in/api/users")
+
+      setUsers(response.data.data)
+    }
+loadUsers();
+  }, [])
+
+
+  const onChangeHandler = (text) => {
+    let matches = []
+    if (text.length>0) {
+      matches = users.filter(user =>{
+        const regex = new RegExp(`${text}`, "gi");
+        return users.email.match(regex)
+      }
+        )
+    }
+    console.log("matches", matches)
+    setSuggestions(matches)
+    setText(text)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+  
+      <input type="text" 
+      className='col-md-12 input'
+      style={{marginTop: 10}}
+      onChange={e => onChangeHandler(e.target.value)}
+      value={text}
+      />
+
     </div>
   );
 }
